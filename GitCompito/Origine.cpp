9337f5e47a04b208194b365a5fe3e8879c46266c
Stealth-Fox <sergio_suteu@yahoo.it>
2018-12-04 10:16:51 +0100

    Modifica logica

--- GitCompito/Origine.cpp
+++ GitCompito/Origine.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-	for (int i = 0; i < 10; i++) //errore inserito di proposito
+	for (int i = 0; i < 12; i++) //errore inserito di proposito
 	{
 		cout << "Contatore = " << i << endl;
 	}
