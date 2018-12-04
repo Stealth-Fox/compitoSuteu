f186f59e6a713f52d90797668bbb9dc3df09e24b
Stealth-Fox <sergio_suteu@yahoo.it>
2018-12-04 09:56:32 +0100

    Primo commit con errori di proposito logici

---
+++ GitCompito/Origine.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for (int i = 0; i < 10; i++) //errore inserito di proposito
+	{
+		cout << "Contatore = " << i;
+		system("PAUSE"); //noia inserita di proposito
+	}
+}
